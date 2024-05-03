75b945dc864ffece9d44e903e54ecb561da465c5
iagokrt <iago_floripa@hotmail.com>
2024-05-03 00:16:23 -0300

    first server

---
+++ node/fundamentos/src/server.js
@@ -0,0 +1,10 @@
+// const http = require('http'); // console.log(http)
+import http from 'node:http'; // console.log(http)
+
+// const server = http.createServer((req, res) => {
+//   console.log('Hello World')
+//   return res.end("Server is up!")
+// });
+
+// server.listen(3000);
+
