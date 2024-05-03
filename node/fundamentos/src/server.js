d21dcebb38a4064e74cac542ec62005628d02e95
iagokrt <iago_floripa@hotmail.com>
2024-05-03 00:24:43 -0300

    nodemon

--- node/fundamentos/src/server.js
+++ node/fundamentos/src/server.js
@@ -1,10 +1,10 @@
 // const http = require('http'); // console.log(http)
 import http from 'node:http'; // console.log(http)
 
-// const server = http.createServer((req, res) => {
-//   console.log('Hello World')
-//   return res.end("Server is up!")
-// });
+const server = http.createServer((req, res) => {
+  console.log('Hello World')
+  return res.end("Server is running!")
+});
 
-// server.listen(3000);
+server.listen(3000);
 
