bab05f0c8e1cb5fce68214a6882334b2ca829345
iagokrt <iago_floripa@hotmail.com>
2024-05-12 18:55:19 -0300

    cookies

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,10 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
-import { env } from './env'
+import cookie from '@fastify/cookie'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
+
+app.register(cookie)
 
 app.register(transactionsRoutes, {
   prefix: '/transactions'
