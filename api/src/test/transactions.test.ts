e956547aa20f888cbda090cbb783e73e7426548d
iagokrt <iago_floripa@hotmail.com>
2024-05-15 20:17:53 -0300

    e2e tests

--- api/src/test/transactions.test.ts
+++ api/src/test/transactions.test.ts
@@ -54,4 +54,66 @@ describe('Transactions routes', () => {
       })
     ])
   })
+
+  test('GET - User can list transaction by Id', async () => {
+    const createResponse = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'Transação Teste',
+      amount: 5000,
+      type: 'credit'
+    })
+    
+    // retrieve cookies (for authentication)
+    const cookies  = createResponse.get('Set-Cookie') ?? []
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    // transaction id
+    const id = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+    .get(`/transactions/${id}`)
+    .set('Cookie', cookies)
+    .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'Transação Teste', 
+        amount: 5000
+      })
+    )
+  })
+
+  test('GET - User can get summary', async () => {
+    const createResponse = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'Transação Teste 1',
+      amount: 5000,
+      type: 'credit'
+    })
+    
+    const cookies  = createResponse.get('Set-Cookie') ?? []
+
+    await request(app.server)
+    .post('/transactions').set('Cookie', cookies)
+    .send({
+      title: 'Transação Teste 2',
+      amount: 2000,
+      type: 'debit'
+    })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000
+    })
+  })
 })
